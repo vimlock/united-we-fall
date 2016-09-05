8e909961bb4fe234ebb0c73042be0ec88dc3507d
A-P <ari-pekka.attman@cou.fi>
2016-09-05 13:43:18 +0300

    kommitti

---
+++ Assets/scripts/enemyHitCounter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyHitCounter : MonoBehaviour {
+
+    public int osumaluku = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        osumaluku++;
+        kuoleeko();
+    }
+
+    void kuoleeko()
+    {
+        if (osumaluku > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
