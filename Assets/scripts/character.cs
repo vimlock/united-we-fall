5790950744ad20fae93403bc77e917ce4f6c1486
Masapa <matti@passoja.fi>
2016-09-05 15:09:20 +0300

    Controlleri päivitetty
    Characterissa pyörimis

---
+++ Assets/scripts/character.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character : MonoBehaviour {
+	//ukko1.transform.rotation = Quaternion.AngleAxis (angleL, Vector3.forward);
+	// Use this for initialization
+
+	public int player = 1;
+	public GamePadController controller;
+	void Start () {
+		controller = GameObject.FindGameObjectWithTag("GamePad").GetComponent<GamePadController>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player == 1) {
+			if (!controller.deadzoneL) {
+				transform.rotation = Quaternion.AngleAxis (controller.angleL, Vector3.forward);
+			}
+		} else {
+			if (controller.deadzoneR) {
+				transform.rotation = Quaternion.AngleAxis (controller.angleR, Vector3.forward);
+			}
+		}
+
+
+	}
+}
