ff960f13c4ba779f9ca39722591eaf4837bbdf0e
Joel Polso NTIS15K <Joel.Polso@centria.fi>
2016-09-05 13:06:57 +0300

    Some base structure for the project

---
+++ Assets/Scripts/EnemySpawnerBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnerBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("script workin");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
