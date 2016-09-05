30159ff27f2031b5138663464708288f3a5682d3
Joel Polso <joel.polso@gmail.com>
2016-09-05 22:34:54 +0300

    Added IController which can be used to abstract away the actual controller and a keyboard controller

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,13 +3,40 @@ using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
 
+    // Should probably think of a better name for this.
+    public enum PlayerId {
+        LEFT,
+        RIGHT
+    };
+
+    // The controller component.
+    public IController controller;
+
+    // Which one of the players this components is.
+    // Should be assigned by a prefab or something else.
+    public PlayerId id;
+
 	// Use this for initialization
-	void Start () {
-	
+    void Start ()
+    {
+        if (controller == null) {
+            Debug.LogError("PlayerBehaviour does not have IController component assigned");
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        // No controller? We're out of luck then.
+        if (controller == null) {
+            return;
+        }
+
+        if (id == PlayerId.LEFT) {
+            transform.rotation = Quaternion.AngleAxis (controller.angleLeft, Vector3.forward);
+        }
+        else if (id == PlayerId.RIGHT){
+            transform.rotation = Quaternion.AngleAxis (controller.angleRight, Vector3.forward);
+        }
+    }
 }
